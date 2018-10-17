c983343225b014f7eca0c2c42b5199540b531e70
Juan Ignacio Cabrera <juanignaciocabrera@live.com>
2018-10-17 12:44:14 -0300

    Prueba de test con junit

---
+++ TPMetodosAgiles/src/test/java/Logica/LicenciaControllerTest.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import Persistencia.Licencia;
+import Persistencia.Persona;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JIC
+ */
+public class LicenciaControllerTest {
+    
+    public LicenciaControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLicencias method, of class LicenciaController.
+     */
+    @Test
+    public void testGetLicencias() {
+        System.out.println("getLicencias");
+        LicenciaController instance = new LicenciaController();
+        List<Licencia> expResult = null;
+        List<Licencia> result = instance.getLicencias();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersona method, of class LicenciaController.
+     */
+    @Test
+    public void testGetPersona() {
+        System.out.println("getPersona");
+        int id = 0;
+        LicenciaController instance = new LicenciaController();
+        Persona expResult = null;
+        Persona result = instance.getPersona(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getVigencia method, of class LicenciaController.
+     */
+    @Test
+    public void testGetVigencia() {
+        System.out.println("getVigencia");
+        Licencia licencia = null;
+        LicenciaController instance = new LicenciaController();
+        int expResult = 0;
+        int result = instance.getVigencia(licencia);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
