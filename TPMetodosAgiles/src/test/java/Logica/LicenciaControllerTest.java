2f9ee9b7a10ff1784693a0989d5a5b5ee9bfdb63
Alejandro <ale_berutti@hotmail.com>
2018-12-06 09:40:54 -0300

    Test LicenciaController agregado

--- TPMetodosAgiles/src/test/java/Logica/LicenciaControllerTest.java
+++ TPMetodosAgiles/src/test/java/Logica/LicenciaControllerTest.java
@@ -5,26 +5,30 @@
  */
 package Logica;
 
+import Logica.LicenciaController.Motivo;
 import Persistencia.Licencia;
 import Persistencia.Persona;
+import Persistencia.Vigencias;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 /**
  *
- * @author JIC
+ * @author ale_b
  */
 public class LicenciaControllerTest {
     
-    public LicenciaControllerTest() {
-    }
+    VigenciaController vigenciaController = new VigenciaController();
+    LicenciaController licenciaController = new LicenciaController();
+    
     
     @BeforeClass
     public static void setUpClass() {
@@ -45,45 +49,126 @@ public class LicenciaControllerTest {
     /**
      * Test of getLicencias method, of class LicenciaController.
      */
-    @Ignore
-    public void testGetLicencias() {
-        System.out.println("getLicencias");
-        LicenciaController instance = new LicenciaController();
-        List<Licencia> expResult = null;
-        List<Licencia> result = instance.getLicencias();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @Test
+    public void LicenciaControllerTest() {
+        Vigencias vigencia= new Vigencias();
+        Date fechaV = new Date();
+        Calendar fechaHoy= Calendar.getInstance();
+        
+       String condicion1 = "primera_menor_21";
+       String condicion2 = "segunda_menor_21";
+       String condicion3 = "menor_46";
+       String condicion4 = "menor_60";
+       String condicion5 = "menor_70";
+       String condicion6 = "mayor_70";
+       
+       int anio1= 1;
+       int anio3= 3;
+       int anio4= 4;
+       int anio5= 5;
+       
+       Calendar fecha = Calendar.getInstance();
+       
+       fecha.set(2000, 0, 30);
+       vigencia = vigenciaController.getVigencia(Motivo.ORIGINAL.toString(), fecha.getTime());
+       assertEquals(condicion1, vigencia.getCondicion());
+       assertEquals(anio1, vigencia.getDuracion());
+       Assert.assertFalse(anio3==vigencia.getDuracion());
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       vigencia = vigenciaController.getVigencia(Motivo.RENOVACION.toString(), fecha.getTime());
+       assertEquals(condicion2, vigencia.getCondicion());
+       assertEquals(anio3, vigencia.getDuracion());
+       
+       fechaHoy = Calendar.getInstance();
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       fecha.set(1985, 5, 12);
+       vigencia = vigenciaController.getVigencia(Motivo.RENOVACION.toString(), fecha.getTime());
+       assertEquals(condicion3, vigencia.getCondicion());
+       assertEquals(anio5, vigencia.getDuracion());
+       
+       fechaHoy = Calendar.getInstance();
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       fecha.set(1960, 8, 25);
+       vigencia = vigenciaController.getVigencia(Motivo.RENOVACION.toString(), fecha.getTime());
+       assertEquals(condicion4, vigencia.getCondicion());
+       assertEquals(anio4, vigencia.getDuracion());
+       
+       fechaHoy = Calendar.getInstance();
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       fecha.set(1950, 11, 10);
+       vigencia = vigenciaController.getVigencia(Motivo.RENOVACION.toString(), fecha.getTime());
+       assertEquals(condicion5, vigencia.getCondicion());
+       assertEquals(anio3, vigencia.getDuracion());
+       
+       fechaHoy = Calendar.getInstance();
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       fecha.set(1945, 4, 15);
+       vigencia = vigenciaController.getVigencia(Motivo.RENOVACION.toString(), fecha.getTime());
+       assertEquals(condicion6, vigencia.getCondicion());
+       assertEquals(anio1, vigencia.getDuracion());
+       
+       fechaHoy = Calendar.getInstance();
+       
+       fechaV=licenciaController.getFechaVigencia(vigencia, fecha.getTime());
+       fechaHoy.add(Calendar.YEAR, vigencia.getDuracion());
+       fechaHoy.set(Calendar.DAY_OF_MONTH, fecha.get(Calendar.DAY_OF_MONTH));
+       fechaHoy.set(Calendar.MONTH, fecha.get(Calendar.MONTH));
+       assertEquals(fechaHoy.getTime().getYear(), fechaV.getYear());
+       assertEquals(fechaHoy.getTime().getMonth(), fechaV.getMonth());
+       assertEquals(fechaHoy.getTime().getDay(), fechaV.getDay());
+       
+       
+       assertEquals(true, licenciaController.validarClaseNuevoTitular("A"));
+       assertEquals(true, licenciaController.validarClaseNuevoTitular("B"));
+       assertEquals(true, licenciaController.validarClaseNuevoTitular("F"));
+       assertEquals(true, licenciaController.validarClaseNuevoTitular("G"));
+       assertEquals(false, licenciaController.validarClaseNuevoTitular("C"));
+       assertEquals(false, licenciaController.validarClaseNuevoTitular("D"));
+       assertEquals(false, licenciaController.validarClaseNuevoTitular("E"));
+       
+       
     }
 
-    /**
-     * Test of getPersona method, of class LicenciaController.
-     */
-    @Ignore
-    public void testGetPersona() {
-        System.out.println("getPersona");
-        int id = 0;
-        LicenciaController instance = new LicenciaController();
-        Persona expResult = null;
-        Persona result = instance.getPersona(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getVigencia method, of class LicenciaController.
-     */
-    @Ignore
-    public void testGetVigencia() {
-        System.out.println("getVigencia");
-        Licencia licencia = null;
-        LicenciaController instance = new LicenciaController();
-        int expResult = 0;
-//        Date result = instance.getVigencia(licencia);
-//        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    
     
 }
